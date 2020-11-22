3fd124c3121f661084eaee61e5d848e565fc5ab1
NikoPythonProg <61151312+NikoPythonProg@users.noreply.github.com>
2020-11-22 21:46:06 +0100

    Create 004_Nico_M.java

---
+++ musculation/004_liste_courses_quantite/solutions/004_Nico_M.java
@@ -0,0 +1,52 @@
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;//pour un dictionnaire!
+import java.util.*; 
+
+
+public class ReadTextFile
+{
+	public static void main (String[] args) throws IOException
+		{
+			BufferedReader in = new BufferedReader(new FileReader("C:/Mes_Auto_Formations/java/musculation/ex004/liste_avec_doublons.txt"));
+			String line;
+            int i = 0;
+            Map<String, Integer> dictionnaire = new TreeMap<String, Integer>();
+            while ((line = in.readLine()) != null)
+
+			{
+		      // Afficher le contenu du fichier
+                line = line.toUpperCase();
+                line = line.trim();
+                line = line.replace("  "," ");
+                line = line.replace(" :",":");
+                line = line.replace(": ",":");
+                line = line.replace(" : ",":");
+                String listeSplit[] ;
+                if (line.contains(":"))
+                {
+                    listeSplit = line.split(":");
+                }
+                else 
+                {
+                    continue;
+                }
+                i=i+1;
+                System.out.println(listeSplit[0]);
+                if (dictionnaire.containsKey(listeSplit[0]))
+                {
+                    dictionnaire.replace(listeSplit[0], dictionnaire.get(listeSplit[0])+Integer.valueOf((listeSplit[1])));
+                }
+                else
+                {
+                    dictionnaire.put(listeSplit[0], Integer.valueOf((listeSplit[1])));
+                }
+                System.out.println(dictionnaire);
+            }
+
+        }
+
+}
